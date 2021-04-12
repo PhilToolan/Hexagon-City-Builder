6228bcfbfe68cea2792272d1deadde268a4f792c
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2021-04-12 21:12:39 +0100

    day 1 finished

--- Assets/Scripts/HexMapEditor.cs
+++ Assets/Scripts/HexMapEditor.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class HexMapEditor : MonoBehaviour
 {
@@ -16,7 +17,7 @@ public class HexMapEditor : MonoBehaviour
 
 	void Update()
 	{
-		if (Input.GetMouseButton(0))
+		if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
 		{
 			HandleInput();
 		}
